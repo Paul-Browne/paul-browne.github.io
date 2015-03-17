38f5788319ea375104c3a3915aa0fbca25225bd4
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-03-17 15:07:44 +0200

    Update compiled.js

--- curator/j/compiled.js
+++ curator/j/compiled.js
@@ -335,8 +335,10 @@ var hljs=new function(){function k(v){return v.replace(/&/gm,"&amp;").replace(/<
       }
       var justName = document.querySelectorAll(".user span span");
       if (justName.length){
+        for(var i=0; i<justName.length; ++i) {
           var nameAndBadge = document.querySelectorAll(".user span");
           nameAndBadge[0].innerHTML = justName[0].innerHTML;
+        }
       }
     }
     
