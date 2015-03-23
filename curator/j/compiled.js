cb2ce1b4864c6b4847e5b5dc936bccfc0d274d0d
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-03-23 10:25:45 +0200

    Update compiled.js

--- curator/j/compiled.js
+++ curator/j/compiled.js
@@ -4,7 +4,7 @@
 // https://github.com/d4tocchini/customevent-polyfill
 // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent#Polyfill
 
-if ((!window.CustomEvent || typeof window.CustomEvent !== 'function')&&!( 'WebkitAppearance' in document.documentElement.style)) {
+if (!window.CustomEvent || typeof window.CustomEvent !== 'function') {
     var CustomEvent = function(event, params) {
         var evt;
         params = params || {
