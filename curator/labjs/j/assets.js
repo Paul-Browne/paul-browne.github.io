fdd29e4b109626afebf27e6fcec20c57b0479323
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-04-02 14:03:32 +0300

    Update assets.js

--- curator/labjs/j/assets.js
+++ curator/labjs/j/assets.js
@@ -5,6 +5,10 @@ $LAB
 	else {return null;}
 })
 .script(function(){
+	if (document.querySelectorAll('pre code').length) {return ["j/highlight.js", "j/highlight.css.js"]; }
+	else {return null;}
+})
+.script(function(){
 	if (document.querySelectorAll('[data-collapse]').length) {return ["//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js", "j/collapse.js"]; }	
 	else {return null;}
 })
