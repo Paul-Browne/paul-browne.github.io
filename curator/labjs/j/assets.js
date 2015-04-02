4b21d16dc7227b5de4259e71523dd67e4cb58e12
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-04-02 13:56:42 +0300

    Update assets.js

--- curator/labjs/j/assets.js
+++ curator/labjs/j/assets.js
@@ -8,6 +8,13 @@ $LAB
 	if (document.querySelectorAll('[data-collapse]').length) {return ["//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js", "j/collapse.js"]; }	
 	else {return null;}
 })
+.script(function(){
+	if (document.querySelectorAll('[type="checkbox"],[type="radio"]').length) {return ["//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js", "j/icheck.js"]; }	
+	else {return null;}
+})
 .wait(function(){
   epicGrid();
+})
+.wait(function(){
+  $('input').iCheck({checkboxClass: 'icheckbox_flat-blue',radioClass: 'iradio_flat-blue'});
 });
