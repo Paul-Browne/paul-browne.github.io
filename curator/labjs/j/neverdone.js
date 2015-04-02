3f4088f92bbaf318c5f1a7c6a1fa2c3a02745a81
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-04-02 18:54:32 +0300

    Delete neverdone.js

--- curator/labjs/j/neverdone.js
+++
@@ -1,6 +0,0 @@
-setTimeout(
-  function(){
-    var script = document.createElement("script");
-    script.src = "j/neverdone.js";
-    document.getElementsByTagName("head")[0].appendChild(script);
-  },3e3)
