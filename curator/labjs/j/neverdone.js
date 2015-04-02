fddb9e376482c2ac1764206617d522e4f0297353
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-04-02 18:51:32 +0300

    Update neverdone.js

--- curator/labjs/j/neverdone.js
+++ curator/labjs/j/neverdone.js
@@ -1,3 +1,6 @@
-var script = document.createElement("script");
-script.src = "j/neverdone.js";
-document.getElementsByTagName("head")[0].appendChild(script);
+setTimeout(
+  function(){
+    var script = document.createElement("script");
+    script.src = "j/neverdone.js";
+    document.getElementsByTagName("head")[0].appendChild(script);
+  },1e3)
