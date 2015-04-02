fd9ce64c9cb84dad15022e5265ac6955174342b9
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-04-02 13:41:49 +0300

    Update assets.js

--- curator/labjs/j/assets.js
+++ curator/labjs/j/assets.js
@@ -4,6 +4,10 @@ $LAB
 	if (document.querySelectorAll('[data-sortable]').length) {return "j/sortable.js"; }
 	else {return null;}
 })
+.script(function(){
+	if (document.querySelectorAll('[data-collapse]').length) {return ["j/collapse.js", "//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"]; }	
+	else {return null;}
+})
 .wait(function(){
   epicGrid();
 });
