066fbba56de0ebee73eba86cd4843fbd50fd3aff
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-04-27 17:06:10 +0300

    Update inits.js

--- js/inits.js
+++ js/inits.js
@@ -7,9 +7,11 @@ $(function() {
             duration: 500,
             render: function () {
                 $('body').addClass('byebye');
+                $('body').removeClass('hihi');
             }
         },
         callback: function(){
+            $('body').addClass('hihi');
             $('body').removeClass('byebye');
             epicGrid();
         }
