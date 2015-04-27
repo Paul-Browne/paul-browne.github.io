9e81b65dbe49fe67282ed908abb0869f1530e63d
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-04-27 17:03:40 +0300

    Update inits.js

--- js/inits.js
+++ js/inits.js
@@ -4,17 +4,9 @@ $(function() {
         prefetch: true,
         pageCacheSize: 1,
         onStart: {
-            duration: 250,
+            duration: 500,
             render: function () {
                 $('body').addClass('byebye');
-                $('body').removeClass('hello');
-            }
-        },
-        onEnd: {
-            duration: 250,
-            render: function () {
-                $('body').addClass('hello');
-                $('body').removeClass('byebye');
             }
         },
         callback: function(){
