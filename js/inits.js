cecfb48a24b33527f9feafa92bb9e8981d3246da
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-04-27 16:44:25 +0300

    Update inits.js

--- js/inits.js
+++ js/inits.js
@@ -3,6 +3,7 @@ $(function() {
     $('#main').smoothState({
         prefetch: true,
         pageCacheSize: 1,
+        onStart: { $('body').addClass('start'); },
         callback: function(){epicGrid()}
     });
 });
