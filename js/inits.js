5f414f062faf67ee25de04def94f2b69b80b90db
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-04-27 17:16:35 +0300

    Update inits.js

--- js/inits.js
+++ js/inits.js
@@ -4,7 +4,7 @@ $(function() {
         prefetch: true,
         pageCacheSize: 1,
         onStart: {
-            duration: 400,
+            duration: 500,
             render: function () {
                 $('body').addClass('byebye');
                 $('body').removeClass('hihi');
