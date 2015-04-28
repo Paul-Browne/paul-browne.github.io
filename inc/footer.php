4946cf7a074884b3d446206640520b96875d78a7
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-04-28 20:14:50 +0300

    Delete footer.php

--- inc/footer.php
+++
@@ -1,7 +0,0 @@
-<?php echo'
-				</div><!-- /main -->
-        <div id="jsorg">
-            <a href="http://js.org" target="_blank" title="JS.ORG | JavaScript Community">
-            <img src="http://logo.js.org/dark_vert.png" width="50" alt="JS.ORG Logo"/></a>
-        </div>
-';?>
