b0107cab09d3bf3b6d96f9a7721d0dc3574e7ec5
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-04-28 09:30:46 +0300

    Update inits.js

--- js/inits.js
+++ js/inits.js
@@ -3,18 +3,19 @@ $(function() {
     $('#main').smoothState({
         prefetch: true,
         pageCacheSize: 1,
+        onEnd: {
+            duration: 250,
+            render: function (url, $container) {
+                $('body').addClass('hihi');
+                $('body').removeClass('byebye');
+                epicGrid();
+            }
+        },
         onStart: {
             duration: 250,
             render: function (url, $container) {
                 $('body').addClass('byebye');
                 $('body').removeClass('hihi');
-            }
-        },
-        onEnd: {
-            render: function (url, $container) {
-                $('body').addClass('hihi');
-                $('body').removeClass('byebye');
-                epicGrid();
             }
         }
     });
