7a813cc6db812ce85f89a5be8088cced0b2cab37
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-04-28 19:59:42 +0300

    Create nav.php

---
+++ inc/nav.php
@@ -0,0 +1,8 @@
+<?php echo'
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="page2.php">About</a></li>
+            </ul>
+        </nav>
+';?>
