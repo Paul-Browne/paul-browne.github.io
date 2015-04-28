f250e98da16a3426c94f1c8c5d215d7ded04bd79
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-04-28 19:55:38 +0300

    Create head.js

---
+++ inc/head.js
@@ -0,0 +1,14 @@
+<?php echo'
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="//fonts.googleapis.com/css?family=Open+Sans:400,600|Libre+Baskerville:400italic" rel="stylesheet" type="text/css">
+        <link href="/css/style.css" type="text/css" media="all" rel="stylesheet">
+        <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css" media="all" rel="stylesheet">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="/js/scripts.js"></script>
+        <!--[if lt IE 9]><script src="https://cdn.rawgit.com/aFarkas/html5shiv/master/dist/html5shiv.min.js"></script><![endif]-->
+';?>
