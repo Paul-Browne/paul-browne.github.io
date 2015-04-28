1ec8f5d0430753e63c879eac1bf5635a4c030336
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-04-28 10:15:11 +0300

    Update inits.js

--- js/inits.js
+++ js/inits.js
@@ -12,8 +12,8 @@ $(function() {
         },
         callback: function(){
             epicGrid();
-            $('#main').addClass('hihi').delay(1000);
-            $('#main').removeClass('byebye'),delay(1000);
+            $('#main').delay(100).addClass('hihi');
+            $('#main').delay(100).removeClass('byebye');
         }
     });
 });
