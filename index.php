2c5483b672d13f67d492ca884792bf842050b6bb
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-04-28 19:54:26 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php include 'head.php';?>
+<title>Pages title</title>
+<?php include 'header.php';?>
+<?php include 'nav.php';?>
+
+            <div class="l-center-5-6">
+                <div class="l-col-2-12 me-cont">
+                    <img class="valign-h2" src = "/images/me-plhldr-240.jpg"
+                    data-src = "<240:/images/me-240.jpg,
+                                <360:/images/me-360.jpg,
+                                <480:/images/me-480.jpg,
+                                <600:/images/me-600.jpg,
+                                >720:/images/me-720.jpg" />
+                <div class="center-text">
+                    <span class="fa fa-2x fa-github-square"></span>&nbsp;&nbsp;
+                    <span class="fa fa-2x fa-twitter-square"></span>&nbsp;&nbsp;
+                    <span class="fa fa-2x fa-facebook-square"></span>
+                </div>
+                </div>
+                <div class="l-col-19-24 l-offset-1-24">
+                    <div id="post">
+                        <h2>Hello World</h2>
+                        <p>April, 25th 2015</p>
+                        <p>So, I'm very excited to have secured the sub-domain <code>paul.js.org</code> as part of the fantastic <a href="http://js.org" target="_blank" title="JS.ORG | JavaScript Community">js.org</a> project - giving away free sub-domains to any javascript developer looking for a place to host their <a href="https://github.com" target="_blank" title="github">github</a> projects.</p>
+                        <p>I'll be treating this domain as a showcase for all my javascript projects, as well using it as a sort of blog, sharing snippets and mentioning any other peoples projects that look interesting. <em>Stay tuned!</em></p>
+                    </div>
+                </div>
+            </div>
+
+<?php include 'footer.php';?>
+<script src="/js/inits.js"></script>
+<?php include 'close.php';?>
